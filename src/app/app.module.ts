d0c2167dab2af02be967ee26177a695e2ee63903
jasiekmarc <jasiekmarc@gmail.com>
2017-08-20 02:51:48 +0200

    Using material design and flex-layout provided by the Angular team.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeafletModule } from '@asymmetrik/angular2-leaflet';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MdButtonModule,
+  MdDialogModule,
+  MdIconModule,
+  MdSliderModule,
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { MapComponent } from '../components/map/map';
+import { JoystickComponent } from '../components/joystick/joystick';
 
 import { TrackService } from "../providers/track.state";
+import { LayerService } from "../providers/layer.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    JoystickComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     LeafletModule.forRoot(),
+
+    // Material Components
+    FlexLayoutModule,
+    MdButtonModule,
+    MdDialogModule,
+    MdIconModule,
+    MdSliderModule,
   ],
   providers: [
     TrackService,
+    LayerService,
   ],
   bootstrap: [
     AppComponent,
